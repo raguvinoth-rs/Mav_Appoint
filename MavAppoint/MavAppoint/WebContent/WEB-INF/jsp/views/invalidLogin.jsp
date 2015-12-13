05ae1ff16fbadd6d8d373fbca27ee5c21c85dbed
raguvioth-rs <raguvinoth08@gmail.com>
2015-12-13 16:05:37 -0600

    Changing to temp password for wrong password limit

---
+++ MavAppoint/MavAppoint/WebContent/WEB-INF/jsp/views/invalidLogin.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ include file="templates/header.jsp" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Invalid Login</title>
+
+<style>
+.panel-heading {
+    padding: 5px 15px;
+}
+
+.panel-footer {
+	padding: 1px 15px;
+	color: #A0A0A0;
+}
+
+.profile-img {
+	width: 96px;
+	height: 96px;
+	margin: 0 auto 10px;
+	display: block;
+	-moz-border-radius: 50%;
+	-webkit-border-radius: 50%;
+	border-radius: 50%;
+}
+</style>
+</head>
+<body>
+<b><font size=3 color="white">
+There were 3 failed password attempts made. The Password has been changed and Mail sent to the respective Email Address. 
+Please login with the new password</font></b>
+		</body>
+</html>
--- MavAppoint/MavAppoint/servlets/uta/mav/appoint/LoginServlet.java
+++ MavAppoint/MavAppoint/servlets/uta/mav/appoint/LoginServlet.java
@@ -1,7 +1,11 @@
 package uta.mav.appoint;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,6 +25,23 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	HttpSession session;
 	int loginAttempt;
+	public Connection connectDB(){
+		try
+	    {
+	    Class.forName("com.mysql.jdbc.Driver").newInstance();
+	    String jdbcUrl = "jdbc:mysql://localhost:3306/MavAppointDB";
+	    String userid = "root";
+	    String password = "willpower";
+	    Connection conn = DriverManager.getConnection(jdbcUrl,userid,password);
+	    System.out.println("connection status"+conn);
+	    return conn;
+	    }
+	    catch (Exception e){
+	        System.out.println(e.toString());
+	    }
+	    return null;
+	}
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -66,10 +87,25 @@ public class LoginServlet extends HttpServlet {
 				session.setAttribute("loginCount", loginAttempt++);
 				if(loginAttempt > 2){
 					System.out.println("Number of Failed Attempts: "+loginAttempt);
-					response.sendRedirect("index");
+					System.out.println("There were 3 failed password attempts made");
+					//response.sendRedirect("forgetPassword.html?var1="+emailAddress+"");
+					//response.sendRedirect("index");
+					
+					Connection conn = connectDB();
+					PreparedStatement statement;
+					String command = "UPDATE mavappointdb.user SET USERPASSWORD='Newpass12!' WHERE EMAIL=?";
+					//String command = "UPDATE mavappointdb.user SET VALIDATED=0 WHERE EMAIL=?";
+					statement=conn.prepareStatement(command);
+					statement.setString(1, emailAddress);
+					statement.executeUpdate();
+					conn.close();
+					System.out.println("The Password for the Email Address "+emailAddress+" has been changed and Mail sent to the respective Email Address");
+					RequestDispatcher requestDispatcher = request
+		                    .getRequestDispatcher("/WEB-INF/jsp/views/invalidLogin.jsp");
+		            requestDispatcher.forward(request, response);
 				}
 				else{
-					response.sendRedirect("login");
+					request.getRequestDispatcher("/WEB-INF/jsp/views/login.jsp").forward(request,response);
 				}
 			}
 		}
